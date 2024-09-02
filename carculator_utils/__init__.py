a3ce5c64138f039ac1419236a9c065f8419f60a6
romainsacchi <r_s@me.com>
2024-09-02 11:50:22 +0200

    Version bump

--- carculator_utils/__init__.py
+++ carculator_utils/__init__.py
@@ -18,7 +18,7 @@ __all__ = (
     "ExportInventory",
     "VehicleInputParameters",
 )
-__version__ = (1, 2, 3)
+__version__ = (1, 3, 0, "dev0")
 
 from pathlib import Path
 
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="carculator_utils",
-    version="1.2.3",
+    version="1.3.0.dev0",
     packages=packages,
     author="Romain Sacchi <romain.sacchi@psi.ch>",
     python_requires=">=3.10",
